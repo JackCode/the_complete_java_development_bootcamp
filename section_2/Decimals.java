f05a1e28ca3780cb46f91b21daa201e645d489d1
Jackson Weber <jackson@jackcode.com>
2023-01-08 15:16:38 -0500

    doubles

---
+++ section_2/Decimals.java
@@ -0,0 +1,10 @@
+public class Decimals {
+  public static void main(String[] args) {
+    double percentage = 73.5;
+    System.out.println(percentage + " of percentages are made up");
+
+    double wallet = 20.0;
+    int people = 3;
+    System.err.println(wallet / people);
+  }
+}
