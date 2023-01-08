ef167682157ec090fdcd046d30840151964f5c9e
Jackson Weber <jackson@jackcode.com>
2023-01-08 15:09:32 -0500

    char and strings

---
+++ section_2/Grades.java
@@ -0,0 +1,11 @@
+public class Grades {
+  public static void main(String[] args) {
+    char potions = 'A';
+    char math = 'B';
+    char charms = 'C';
+
+    System.out.println("Potions: " + potions);
+    System.out.println("Math: " + math);
+    System.out.println("Charms: " + charms);
+  }
+}
